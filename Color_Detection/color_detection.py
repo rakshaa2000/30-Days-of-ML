99ed2168599bad64ee1ddd7da2234fce4255c104
Rakshaa Viswanathan <46165429+rakshaa2000@users.noreply.github.com>
2020-11-10 14:33:26 +0530

    Update color_detection.py

--- Color_Detection/color_detection.py
+++ Color_Detection/color_detection.py
@@ -49,17 +49,13 @@ while(1):
     if (clicked):
         #cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle
         cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
-
         #Creating text string to display( Color name and RGB values )
         text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
-        
         #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
         cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
-
         #For very light colours we will display text in black colour
         if(r+g+b>=600):
             cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
-            
         clicked=False
 
     #Break the loop when user hits 'esc' key
